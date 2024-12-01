1a78a9ae587b1b3e22569a883ec1940dadeee798
sdegroot13 <sophiedegroot4@gmail.com>
2024-12-01 12:12:12 +0000

    linprg with comments.m

--- linprg.m
+++ linprg.m
@@ -1,24 +1,31 @@
-%simplex using linprog
+%linprog function
 
-z = [2 3 4 1 8 1];
-min = false;
+z = [2 3 4 1 8 1]; %objective function
+min = false; %maximisation problem
 
+%setting up the constraints
+%equality constraint
+Aeq = [1 -1 2 0 1 1];
+beq = 18;
+%less than/equal constraints
+A = [0 1 -1 1 0 3; 1 1 -3 1 1 0; 1 -1 0 0 1 1; (-1)*eye(size(z,2))];
+b = [8; 36; 23; zeros([size(z,2) 1])];
+
+%converts maximisation into minimisation problem
 if min == false
     minz = z*(-1);
 else
     minz = z;
 end
 
-Aeq = [1 -1 2 0 1 1];
-beq = [18];
-A = [0 1 -1 1 0 3; 1 1 -3 1 1 0; 1 -1 0 0 1 1; (-1)*eye(6)];
-b = [8; 36; 23; zeros([6 1])];
-
+%linprog function
 x = linprog(minz,A,b,Aeq,beq);
+
+%maximum z value
 sol = z*x;
 
+%displaying the solution
 x = x.';
-
 array2table(x)
 
 if min == false
